09843fca86a0377362e6386a472cd0bd46710aa8
Phạm Đức Bình <binhpd@oeg.vn>
2025-01-03 09:53:55 +0700

    update

---
+++ src/configs/api.ts
@@ -0,0 +1,9 @@
+export const APP_API_ENDPOINT = Object.freeze({
+  AUTH: {
+    LOGIN: "/api/v1/auth/login",
+    REGISTER: "/api/v1/auth/register",
+    LOGIN_GOOGLE: "/api/v1/auth/google",
+    VERIFIED_USER_VALIDATOR: "/api/v1/user/profile",
+    LOG_OUT: "/api/v1/auth/logout",
+  },
+});
---
+++ src/types/auth/index.ts
@@ -0,0 +1,36 @@
+export type TFetchConfig = {
+  method?: string;
+  body?: any;
+  token?: string;
+};
+
+export type TLoginAuth = {
+  username: string;
+  password: string;
+};
+export type TRegisterAuth = {
+  username: string;
+  password: string;
+  passwordConfirm?: string;
+};
+
+export type TUser = {
+  id: number;
+  name: string;
+  email: string;
+  date_of_birth: Date;
+  password?: string;
+  email_verify_token?: string;
+  forgot_password_token?: string;
+  bio: string;
+  location: string;
+  website: string;
+  username: string;
+  cover_photo: string;
+  avatar: string;
+  verify_status: number;
+  following?: any;
+  followers?: any;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
